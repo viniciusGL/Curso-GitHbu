b1280cbd6816316ab34c93621e8692425efb1529
Vinicius Costa <vinicius.costa@globallogic.com>
2024-01-29 13:59:26 -0300

    Agregados archivos de Vinicius

---
+++ Vinicius Costa/Javascript/main.js
@@ -0,0 +1,22 @@
+var miEdad = 25;
+const miNombre = "JUAN TESTE";
+
+var multiplicacion = 5 * 4;
+console.log(multiplicacion);
+
+if (miEdad == 25) {
+    console.log("Tu edad es de 25")
+}
+
+
+let persona = {
+    nombre : "Vinicius Costa",
+    edad : 34,
+    genero : "masculino",
+    mascota :{
+        nombre : "Fido",
+        raza : "Perro"
+    }
+}
+
+console.log(persona.mascota.nombre);
